55d746fefb0fab3190b0510f84f9d25aefc55adc
roberto2021-1 <90346606+roberto2021-1@users.noreply.github.com>
2021-09-08 15:08:25 -0300

    Add files via upload

---
+++ lista01questao01.py
@@ -0,0 +1,6 @@
+#Leia uma velocidade em m/s, calcule e escreva esta velocidade em km/h. (Vkm/h = Vm/s * 3.6)
+velocidade = float(input('Digite a velocidade a ser convertidade em km/h: '))
+
+velocidade_km = velocidade * 3.6
+
+print(f'A velocidade, {velocidade} m/s é =  {velocidade_km} km/h')
